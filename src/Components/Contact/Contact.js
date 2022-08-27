fb58b02f8f1489511240bfc59a055c94ae262119
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-08-28 05:13:06 +0600

    initialize app

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import contactLogo from '../../Images/contact.png'
+const Contact = () => {
+    return (
+        <div className="flex justify-center mt-10">
+            <img src={contactLogo} alt="" />
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import homeLogo from '../../Images/welcome-homepage.png'
+
+
+const Home = () => {
+    return (
+        <div className="flex justify-center mt-24">
+            <img src={homeLogo} alt="" width="800px" />
+        </div>
+    );
+};
+
+export default Home;
