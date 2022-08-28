c64a3b9815ce57d0153ff4ffb0dc015b6a5c421f
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-08-28 10:52:32 +0600

    dashboard structure done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
-import { publicRoutes } from './Routes/publicRoutes';
 import { useState } from 'react';
+import Dashboard from './Components/Dashboard/Dashboard';
 function App() {
   const [dark, setDark] = useState(false);
   return (
     <div data-theme={dark ? "dark" : "light"} style={{ minHeight: "100vh" }}>
       <Navbar dark={dark} setDark={setDark} />
       <Routes>
-        {publicRoutes.map(({ path, Component }, index) => (
-          <Route key={index} path={path} element={<Component />}></Route>
-        ))}
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
       </Routes>
     </div>
   );
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,13 +5,13 @@ import { NavLink } from 'react-router-dom';
 const Navbar = ({ dark, setDark }) => {
 
     return (
-        <div class="navbar bg-base-100" data-theme={dark ? "dark" : "light"}>
-            <div class="navbar-start">
-                <div class="dropdown">
-                    <label tabindex="0" class="btn btn-ghost btn-circle">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+        <div className="navbar bg-base-100" data-theme={dark ? "dark" : "light"}>
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabindex="0" className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         <li>
                             <NavLink to="/">
                                 Home
@@ -27,18 +27,23 @@ const Navbar = ({ dark, setDark }) => {
                                 Contact
                             </NavLink>
                         </li>
+                        <li>
+                            <NavLink to="/dashboard">
+                                Dashboard
+                            </NavLink>
+                        </li>
                     </ul>
                 </div>
             </div>
-            <div class="navbar-center">
-                <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+            <div className="navbar-center">
+                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
             </div>
-            <div class="navbar-end">
-                <label class='swap swap-rotate'>
+            <div className="navbar-end">
+                <label className='swap swap-rotate'>
                     <input type='checkbox' onClick={() => setDark(!dark)} />
 
                     <svg
-                        class='swap-on fill-current w-7 h-7'
+                        className='swap-on fill-current w-7 h-7'
                         xmlns='http://www.w3.org/2000/svg'
                         viewBox='0 0 24 24'
                     >
@@ -46,20 +51,20 @@ const Navbar = ({ dark, setDark }) => {
                     </svg>
 
                     <svg
-                        class='swap-off fill-current w-7 h-7'
+                        className='swap-off fill-current w-7 h-7'
                         xmlns='http://www.w3.org/2000/svg'
                         viewBox='0 0 24 24'
                     >
                         <path d='M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z' />
                     </svg>
                 </label>
-                <button class="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                <button className="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                 </button>
-                <button class="btn btn-ghost btn-circle">
-                    <div class="indicator">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-                        <span class="badge badge-xs badge-primary indicator-item"></span>
+                <button className="btn btn-ghost btn-circle">
+                    <div className="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+                        <span className="badge badge-xs badge-primary indicator-item"></span>
                     </div>
                 </button>
             </div>
