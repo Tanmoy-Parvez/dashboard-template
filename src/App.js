7eac8ae337bff52e273e5486609a4a8a845cfbc0
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-08-30 23:04:54 +0600

    remove test file

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import { useState } from 'react';
 import Dashboard from './Components/Dashboard/Dashboard';
-import Test from './Components/Test/Test';
+
 function App() {
   const [dark, setDark] = useState(false);
   return (
     <div data-theme={dark ? "dark" : "light"} style={{ minHeight: "100vh" }}>
       <Navbar dark={dark} setDark={setDark} />
       <Routes>
-        <Route path="/dashboard" element={<Dashboard/>}></Route>
-        <Route path="/" element={<Test/>}/>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
       </Routes>
     </div>
   );
