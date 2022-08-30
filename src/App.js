1e97729bf545433f5d8ad2dcc8eaa1f2c1077046
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-08-30 15:57:50 +0600

    testing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import { useState } from 'react';
 import Dashboard from './Components/Dashboard/Dashboard';
+import Test from './Components/Test/Test';
 function App() {
   const [dark, setDark] = useState(false);
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar dark={dark} setDark={setDark} />
       <Routes>
         <Route path="/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/" element={<Test/>}/>
       </Routes>
     </div>
   );
